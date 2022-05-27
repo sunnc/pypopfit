490edbf65158caeac5bde2884b0eabbb133e0800
Ning-Chen Sun <nsun@athena.shef.ac.uk>
2022-05-27 17:44:16 +0100

    new

--- pypopfit.py
+++ pypopfit.py
@@ -26,35 +26,25 @@ def like1(synmag, obsmag, err, det):
 	return LL
 
 # prepare the stellar isochrones
-def get_isochrones(iso, LAs, bands=None, DM=None, gal_Av=None, gal_coeff=None, magcuts=None):
+def get_isochrones(iso, LAs, bands=None, DM=None, gal_Av=None, gal_coeff=None):
 #{{{#
 	pts=[]
 	interps=[]
 	for iLA in range(len(LAs)):
 
 		# select isochrone for this age
-		pt0=iso[np.abs(iso['logAge']-LAs[iLA])<1e-4][['Mini']+[bd+'mag' for bd in bands]]
+		pt0=iso[np.abs(iso['logAge']-LAs[iLA])<1e-4][['logAge', 'Mini']+[bd+'mag' for bd in bands]]
 		if len(pt0)==0:
 			print('isochrone of log(t/yr) =', LAs[iLA], 'is not provided in the isochrone table')
 			return -1
-
-		# add galactic extinction and distance modulus to the absolute magnitudes
 		for bd in bands: pt0[bd+'mag']=np.around(pt0[bd+'mag']+gal_coeff[bd]*gal_Av+DM, 3)
+		pts.append(pt0)
 
 		# mass-mag interpolation for this age
 		interp0={}
 		for bd in bands:
 			interp0[bd]=interp1d(pt0['Mini'], pt0[bd+'mag'], fill_value=99.999, bounds_error=False)
 		interps.append(interp0)
-
-		# cut the lower isochrone that is too faint
-		Mlow=999.0
-		for bd in bands:
-			Mlow_tmp=np.amin(pt0[pt0[bd+'mag']<magcuts[bd]]['Mini'])
-			if Mlow_tmp<Mlow: Mlow=Mlow_tmp
-		pt0=pt0[pt0['Mini']>=Mlow]
-
-		pts.append(pt0)
 #}}}#
 	return pts, interps
 
@@ -74,18 +64,15 @@ def get_ABC_maxMs(pts, alpha=-2.35, minStellarMass=0.08, maxStellarMass=300.0):
 #}}}#
 	return AAs, BBs, CC0, maxMs
 
-# interpolate the stellar isochrone to increase sampling in Mini
-def interp_isochrone(pt0, interp0, bands=None, obs=None, dmag_min=0.02):
+# interpolate isochrone
+def interp_isochrone(pt0, interp0, bands=None, dmags=None):
 #{{{#
-	# minimum sampling
-	dmag={bd: np.maximum(dmag_min, obs[bd+'err']) for bd in bands}
-	# new data points
 	Ms_new=[]
 	for iM in range(len(pt0)-1):
 		npts=1 # number of new data points (+1)
 		for bd in bands:
 			delta_mag=np.abs(pt0[iM][bd+'mag']-pt0[iM+1][bd+'mag'])
-			npts_tmp=int(np.ceil(delta_mag/dmag[bd]))
+			npts_tmp=int(np.ceil(delta_mag/dmags[bd]))
 			if npts_tmp>npts: npts=npts_tmp
 		if npts>1:
 			# too sparsely sampled
@@ -95,98 +82,158 @@ def interp_isochrone(pt0, interp0, bands=None, obs=None, dmag_min=0.02):
 	if len(Ms_new)>0:
 		# add new data points with linear interpolation
 		Ms_new=np.hstack(Ms_new)
-		pt0_new=Table([Ms_new], names=['Mini'])
-		for bd in bands: pt0_new[bd+'mag']=np.around(interp0[bd](Ms_new), 3)
-		newpt0=vstack([pt0, pt0_new])
+		newpt0=Table([Ms_new], names=['Mini'])
+		for bd in bands: newpt0[bd+'mag']=np.around(interp0[bd](Ms_new), 3)
+		newpt0=vstack([pt0, newpt0])
 		newpt0=newpt0[np.argsort(newpt0['Mini'])]
 	else:
-		newpt0=pt0.copy()
+		newpt0=pt0
 #}}}#
 	return newpt0
 
+# cut and interpolate the stellar isochrone
+def cut_interp_isochrone(pt0, interp0, bands=None, obs=None, dmag_min=0.02):
+#{{{#
+	# divide the isochrone into the upper and lower branches
+	Mlow=0.0
+	for bd in bands:
+		if obs[bd+'det']==1:
+			idxGood=(pt0[bd+'mag']-2.5*np.log10(2)<=obs[bd+'mag']+5*obs[bd+'err'])
+			if np.any(idxGood):
+				Mlow_tmp=np.amin(pt0[idxGood]['Mini'])
+				if Mlow_tmp>Mlow: Mlow=Mlow_tmp
+			else:
+				# this star is too bright for this isochrone
+				return pt0[idxGood], pt0 # return a zero-length table
+	pt_upper=pt0[pt0['Mini']>=Mlow] # upper branch
+	pt_lower=pt0[pt0['Mini']<Mlow] # lower branch: single stars too faint to match the observations
+
+	# minimum sampling
+	dmags={bd: np.maximum(dmag_min, obs[bd+'err']) for bd in bands}
+
+	# interpolate the upper branch
+	newpt1=interp_isochrone(pt_upper, interp0, bands=bands, dmags=dmags)
+
+	# interpolate the lower branch?
+	Mlow=1000.0
+	Ms=(pt_lower[1:]['Mini']+pt_lower[:-1]['Mini'])/2
+	for bd in bands:
+		binmags=addmag(np.amax(pt_upper[bd+'mag']), pt_lower[bd+'mag'])
+		idxBad=(np.abs(binmags[1:]-binmags[:-1])>dmags[bd])
+		if np.any(idxBad):
+			Mlow_tmp=np.amin(Ms[idxBad])
+			if Mlow_tmp<Mlow: Mlow=Mlow_tmp
+	idx=(pt_lower['Mini']>=Mlow)
+	if np.any(idx):
+		newpt2=vstack([pt_lower[~idx],
+			interp_isochrone(pt_lower[idx], interp0, bands=bands, dmags=dmags), newpt1])
+	else:
+		newpt2=vstack([pt_lower, newpt1])
+#}}}#
+	return newpt1, newpt2
+
 # get the important parameter vectors and the magnitude vector/array
-def get_MST_synmags(pt0, bands, maxM0, AA0, BB0, CC0, Pbin, alpha, minStellarMass):
+def get_ST_synmags(newpt1, newpt2, bands, maxM0, AA0, BB0, CC0,
+	Pbin=0.5, alpha=-2.35, minStellarMass=0.08):
 #{{{#
 	# get vectors for the initial masses and the S and T parameters
-	MMvec = np.array(pt0['Mini'])
+	MMvec = np.array(newpt1['Mini'])
 	SSvec = (1-Pbin)*(MMvec**alpha)/AA0 + Pbin*BB0/CC0/(maxM0-minStellarMass)
 	TTvec = Pbin*(MMvec**alpha)/CC0/(MMvec-minStellarMass)
 
 	# get synthetic magnitudes for single and binary stars
-	synmags_sin={bd: np.array(pt0[bd+'mag']) for bd in bands}
-	synmags_bin={bd: addmag(*np.meshgrid(synmags_sin[bd], synmags_sin[bd], sparse=True)) for bd in bands}
-#}}}#
-	res={'MMvec': MMvec, 'SSvec': SSvec, 'TTvec': TTvec,
-		'synmags_sin': synmags_sin, 'synmags_bin': synmags_bin}
-	return res
-
-# calculate the marginalized likelihood for a given age and extinction
-def get_LH(obs, bands, int_coeff, Av0, MMvec, SSvec, TTvec, synmags_sin, synmags_bin):
-#{{{#
-	# magnitude check
+	synmags_sin={bd: np.array(newpt1[bd+'mag']) for bd in bands}
+	synmags_bin={}
 	for bd in bands:
-		if obs[bd+'det']==1:
-			mag_tmp=obs[bd+'mag']-int_coeff[bd]*Av0
-			err_tmp=obs[bd+'err']
-			if mag_tmp+5.0*err_tmp<np.amin(synmags_sin[bd])-2.5*np.log10(2):
-				# this source is too bright than any possible magnitudes
-				return 0.0
+		mass1, mass2 = np.meshgrid(np.array(newpt1['Mini']), np.array(newpt2['Mini']))
+		synmags_bin[bd]=addmag(*np.meshgrid(np.array(newpt1[bd+'mag']),
+			np.array(newpt2[bd+'mag']), sparse=True))
+		synmags_bin[bd][mass2>mass1]=99.999
+#}}}#
+	return SSvec, TTvec, synmags_sin, synmags_bin
 
-	# color check
+# calculate the marginalized likelihood for a given age and a range of Avs
+def calc_LH_vector(pt0, interp0, Avs, int_coeff, bands, obs,
+	dmag_min, maxM0, AA0, BB0, CC0, Pbin, alpha, minStellarMass):
+#{{{#
+	# cut and interpolate the stellar isochrone
+	newpt1, newpt2 = cut_interp_isochrone(pt0, interp0,
+		bands=bands, obs=obs, dmag_min=dmag_min)
+	if len(newpt1)==0:
+		# this star is too bright for this isochrone
+		return np.zeros(len(Avs), dtype=np.float32)
+
+	# get MST vectors and synthetic magnitudes
+	SSvec, TTvec, synmags_sin, synmags_bin = \
+		get_ST_synmags(newpt1, newpt2, bands, maxM0, AA0, BB0, CC0,
+			Pbin=Pbin, alpha=alpha, minStellarMass=minStellarMass)
+
+	# get minimum magnitudes
+	minmags={bd: np.amin(pt0[bd+'mag'])-2.5*np.log10(2) for bd in bands}
+	# get minimum and maximum colors
+	mincols={}
+	maxcols={}
 	for ibd1 in range(len(bands)-1):
 		bd1=bands[ibd1]
-		mag1=obs[bd1+'mag']-int_coeff[bd1]*Av0
-		err1=obs[bd1+'err']
 		for ibd2 in range(ibd1+1, len(bands)):
 			bd2=bands[ibd2]
-			mag2=obs[bd2+'mag']-int_coeff[bd2]*Av0
-			err2=obs[bd2+'err']
-			if obs[bd1+'det']==1:
-				if mag1-mag2+5.0*np.sqrt(err1**2+err2**2)<np.amin(synmags_sin[bd1]-synmags_sin[bd2]):
-					# this source is too blue than any possible colors
-					return 0.0
-			if obs[bd2+'det']==1:
-				if mag1-mag2-5.0*np.sqrt(err1**2+err2**2)>np.amax(synmags_sin[bd1]-synmags_sin[bd2]):
-					# this source is too red than any possible colors
-					return 0.0
+			mincols[bd1+'-'+bd2]=np.amin(pt0[bd1+'mag']-pt0[bd2+'mag'])
+			maxcols[bd1+'-'+bd2]=np.amax(pt0[bd1+'mag']-pt0[bd2+'mag'])
 
-	# calculate likelihood
-	LH_sin=1.0
-	LH_bin=1.0
-	for bd in bands:
-		LH_sin = LH_sin*like1(synmags_sin[bd],
-			obs[bd+'mag']-int_coeff[bd]*Av0, obs[bd+'err'], obs[bd+'det'])
-		LH_bin = LH_bin*like1(synmags_bin[bd],
-			obs[bd+'mag']-int_coeff[bd]*Av0, obs[bd+'err'], obs[bd+'det'])
-	LH_tot = trapezoid(SSvec*LH_sin+TTvec*0.5*trapezoid(LH_bin, x=MMvec, axis=1), x=MMvec)
+	LH1s=np.zeros(len(Avs), dtype=np.float32)
+	for iAv, Av0 in enumerate(Avs):
+
+		# de-redden the observed magnitudes
+		mag0s={bd: obs[bd+'mag']-int_coeff[bd]*Av0 for bd in bands}
+
+		# magnitude check
+		for bd in bands:
+			if obs[bd+'det']==1:
+				if mag0s[bd]+5.0*obs[bd+'err']<minmags[bd]:
+					# this source is too bright than any possible magnitudes
+					LH1s[iAv]=0.0
+					continue
+
+		# color check
+		for ibd1 in range(len(bands)-1):
+			bd1=bands[ibd1]
+			for ibd2 in range(ibd1+1, len(bands)):
+				bd2=bands[ibd2]
+				c12=mag0s[bd1]-mag0s[bd2]
+				ec12=np.sqrt(obs[bd1+'err']**2+obs[bd2+'err']**2)
+				if obs[bd1+'det']==1:
+					if c12+5.0*ec12<mincols[bd1+'-'+bd2]:
+						# this source is too blue than any possible colors
+						LH1s[iAv]=0.0
+						continue
+				if obs[bd2+'det']==1:
+					if c12-5.0*ec12>maxcols[bd1+'-'+bd2]:
+						# this source is too red than any possible colors
+						LH1s[iAv]=0.0
+						continue
+
+		# calculate likelihood
+		LH_sin=1.0
+		LH_bin=1.0
+		for bd in bands:
+			LH_sin = LH_sin*like1(synmags_sin[bd], mag0s[bd], obs[bd+'err'], obs[bd+'det'])
+			LH_bin = LH_bin*like1(synmags_bin[bd], mag0s[bd], obs[bd+'err'], obs[bd+'det'])
+		# marginalize the likelihood over M1 and M2
+		intdM2=trapezoid(LH_bin, x=np.array(newpt2['Mini']), axis=0)
+		LH1s[iAv]=trapezoid(SSvec*LH_sin+TTvec*intdM2, x=np.array(newpt1['Mini']))
 #}}}#
-	return LH_tot
+	return LH1s
 
 def calc_LH_array(obs, LAs, Avs, pts, interps, AAs, BBs, CC0, maxMs, bands=None, int_coeff=None,
 	Pbin=0.5, alpha=-2.35, minStellarMass=0.08, num_core=1, dmag_min=0.02):
 #{{{#
-	# isochrone interpolation
-	newpts=[interp_isochrone(pts[iLA], interps[iLA], bands=bands, obs=obs,
-		dmag_min=dmag_min) for iLA in range(len(LAs))]
-
-	# calculate the parameter vectors and synthetic magnitudes
 	pool=multiprocessing.Pool(num_core)
-	res = pool.starmap(get_MST_synmags, [(newpts[iLA], bands, maxMs[iLA], AAs[iLA], BBs[iLA], CC0,
+	LHs=pool.starmap(calc_LH_vector, [(pts[iLA], interps[iLA], Avs, int_coeff, bands, obs,
+		dmag_min, maxMs[iLA], AAs[iLA], BBs[iLA], CC0,
 		Pbin, alpha, minStellarMass) for iLA in range(len(LAs))])
 	pool.close()
 	pool.join()
-
-	# calculate likelihood
-	pool=multiprocessing.Pool(num_core)
-	LHs = pool.starmap(get_LH, [(obs, bands, int_coeff, Avs[iAv],
-		res[iLA]['MMvec'], res[iLA]['SSvec'], res[iLA]['TTvec'],
-		res[iLA]['synmags_sin'], res[iLA]['synmags_bin'])
-			for iLA in range(len(LAs)) for iAv in range(len(Avs))]) # LHs[iLA, iAv]
-	pool.close()
-	pool.join()
-
-	# convert the result into a numpy array
-	LHs=np.array(LHs, dtype=np.float32).reshape(len(LAs), len(Avs))
+	LHs=np.vstack(LHs)
 #}}}#
 	return LHs
 
@@ -195,7 +242,7 @@ def starfit(cat_filename=None, iso_filename=None, index=None, bands=None,
 	intAv_min=0.0, intAv_max=2.0, intAv_bin=0.02,
 	DM=None, gal_Av=None, gal_coeff=None, int_coeff=None,
 	Pbin=0.5, alpha=-2.35, minStellarMass=0.08, maxStellarMass=300.0,
-	magcuts=None, dmag_min=0.02, num_core=1, path='./', overwrite=False):
+	dmag_min=0.02, num_core=1, path='./', overwrite=False):
 
 	# check arguments and keywords{{{
 	if (cat_filename is None): print('cat_filename not provided')
@@ -205,12 +252,6 @@ def starfit(cat_filename=None, iso_filename=None, index=None, bands=None,
 	if (gal_Av is None) : print('gal_Av not provided')
 	if (gal_coeff is None): print('gal_coeff not provided')
 	if (int_coeff is None) : print('int_coeff not provided')
-	if (magcuts is None): print('magcuts not provided')
-	# check magnitude cuts
-	for bd in bands:
-		if bd not in magcuts.keys():
-			print('magcuts not provided')
-			return -1
 	# check extinction coefficients
 	for bd in bands:
 		if bd not in gal_coeff.keys():
@@ -277,9 +318,6 @@ def starfit(cat_filename=None, iso_filename=None, index=None, bands=None,
 	print()
 	print('Settings:')
 	print('--------')
-	print('Magnitude cuts:', end='')
-	for bd in bands: print('  '+bd+': '+str(magcuts[bd]), end='')
-	print()
 	print('Minimum magnitude resolution:', dmag_min)
 	print('Number of cores:', num_core)
 	print('Where to save the results:', path)
@@ -295,7 +333,7 @@ def starfit(cat_filename=None, iso_filename=None, index=None, bands=None,
 	# print out information}}}
 
 	pts, interps = get_isochrones(iso, LAs, bands=bands,
-		DM=DM, gal_Av=gal_Av, gal_coeff=gal_coeff, magcuts=magcuts)
+		DM=DM, gal_Av=gal_Av, gal_coeff=gal_coeff)
 	AAs, BBs, CC0, maxMs = get_ABC_maxMs(pts, alpha=alpha,
 		minStellarMass=minStellarMass, maxStellarMass=maxStellarMass)
 
@@ -331,10 +369,9 @@ def starfit(cat_filename=None, iso_filename=None, index=None, bands=None,
 			bands=bands, int_coeff=int_coeff,
 			Pbin=Pbin, alpha=alpha, minStellarMass=minStellarMass,
 			num_core=num_core, dmag_min=dmag_min)
-		# save results
+		# save and plot results
 		with open(resfilename, 'wb') as ff:
 			pickle.dump(LHs, ff)
-		# plot results
 		fig=plt.figure(figsize=[3.5, 3])
 		ax=fig.add_subplot(111)
 		ax.set_title('Star '+str(ii))
@@ -348,7 +385,7 @@ def starfit(cat_filename=None, iso_filename=None, index=None, bands=None,
 		fig.savefig(resfilename.replace('.dat', '.pdf'))
 		plt.close(fig)
 		print(' %7.1f'%(time.time()-time1))
-	print(); print('>>> Complete!')
 	# perform star fit}}}
+	print(); print('>>> Complete!'); print()
 
 # End of file #
--- test.py
+++ test.py
@@ -1,23 +1,15 @@
-import numpy as np
-from astropy.io import ascii
-import matplotlib.pyplot as plt
+import os
 import pypopfit
 
-DM=30.0
-gal_Av=0.0
-gal_coeff={'F275W': 1.989, 'F336W': 1.633, 'F438W': 1.332, 'F555W': 0.989, 'F625W': 0.866, 'F814W': 0.578}
-int_coeff=gal_coeff.copy()
-
-bands=['F275W', 'F336W', 'F438W', 'F555W', 'F625W', 'F814W']
-magcuts={'F275W': 31.0, 'F336W': 31.0, 'F438W': 31.0, 'F555W': 31.0, 'F625W': 31.0, 'F814W': 31.0}
-
-cat_filename='zzcat/cat.dat'
-iso_filename='zziso/iso1.dat'
-
-pypopfit.starfit(cat_filename=cat_filename, iso_filename=iso_filename,
-	index=range(3), bands=bands,
+pypopfit.starfit(cat_filename='../zzcat/cat3.dat',
+	iso_filename='../zziso/iso1.dat',
+	bands=['F275W', 'F336W', 'F438W', 'F555W', 'F625W', 'F814W'],
 	logAge_min=6.60, logAge_max=7.60, logAge_bin=0.01,
-	intAv_min=0.0, intAv_max=2.0, intAv_bin=0.01,
-	DM=DM, gal_Av=gal_Av, gal_coeff=gal_coeff, int_coeff=int_coeff,
-	Pbin=0.5, alpha=-2.35, minStellarMass=0.08, maxStellarMass=300.0,
-	magcuts=magcuts, dmag_min=0.02, num_core=4, path='zzres/', overwrite=True)
+	intAv_min=0.0, intAv_max=0.5, intAv_bin=0.01,
+	DM=30.0, gal_Av=0.1,
+	gal_coeff={'F275W': 1.989, 'F336W': 1.633, 'F438W': 1.332,
+		'F555W': 0.989, 'F625W': 0.866, 'F814W': 0.578},
+	int_coeff={'F275W': 1.989, 'F336W': 1.633, 'F438W': 1.332,
+		'F555W': 0.989, 'F625W': 0.866, 'F814W': 0.578},
+	Pbin=0.5, alpha=-2.35, minStellarMass=0.08, maxStellarMass=100.0,
+	dmag_min=0.02, num_core=50, path='../zzres3/', overwrite=True)
